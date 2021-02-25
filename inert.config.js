92eab50f097730736d391c94f00875695ca1e6b5
Jake <pygamer138@gmail.com>
2021-02-25 09:12:29 +0100

    Create inert.config.js

---
+++ inert.config.js
@@ -0,0 +1,41 @@
+const config = {
+    build: {
+        input: "./posts",
+        output: "./public",
+        posts: "blog",
+        sassEntry: './scss/index.scss',
+        sassFolder: './scss',
+        sassOutput: 'index.css',
+        templates: {
+            home: './templates/home.ejs',
+            post: './templates/post.ejs'
+        }
+    },
+    blogName: `Jake's Blog`,
+    ownerName: `Jake Sarjeant`,
+    description: `Full-stack web developer 👨‍💻, web designer 🎨 & space enthusiast 🚀🛰`,
+    navLinks: [
+        {
+            href: '/',
+            text: 'Home'
+        },
+        {
+            href: 'https://github.com/codemaster138',
+            text: 'GitHub'
+        }
+    ],
+    plugins: ['plugin.js'],
+    assets: './assets',
+    i18n: {
+        languages: {
+            'en': {
+                name: 'English',
+                primaryName: 'English',
+                path: '/'
+            }
+        },
+        primary: 'en'
+    }
+}
+
+module.exports = config;
